d0dee7c5c3e185193b80b7885e797dc11d85dbd7
이용민 <bacaren21@naver.com>
2025-10-29 23:03:22 +0900

    first commit

--- BpTree.h
+++ BpTree.h
@@ -7,29 +7,35 @@
 #include "EmployeeData.h"
 #include "SelectionTree.h"
 
-class BpTree {
+class BpTree
+{
 private:
-	BpTreeNode* root;
-	int	order;		// m children
+	BpTreeNode *root;
+	int order; // m children
 
 public:
-	BpTree(ofstream *fout, int order = 3) {
-		
+	BpTree(ofstream *fout, int order = 3)
+	{
+		// 여기 구현
+		this->root = nullptr;
+		this->order = order;
 	}
-	
-	~BpTree() {
 
+	~BpTree()
+	{
+		// 여기 구현
+		root = nullptr;
 	}
 
 	/* essential */
-	bool		Insert(EmployeeData* newData);
-	bool		excessDataNode(BpTreeNode* pDataNode);
-	bool		excessIndexNode(BpTreeNode* pIndexNode);
-	void		splitDataNode(BpTreeNode* pDataNode);
-	void		splitIndexNode(BpTreeNode* pIndexNode);
-	BpTreeNode* getRoot() { return root; }
-	BpTreeNode* searchDataNode(string name);
-	void		searchRange(string start, string end);
+	bool Insert(EmployeeData *newData);
+	bool excessDataNode(BpTreeNode *pDataNode);
+	bool excessIndexNode(BpTreeNode *pIndexNode);
+	void splitDataNode(BpTreeNode *pDataNode);
+	void splitIndexNode(BpTreeNode *pIndexNode);
+	BpTreeNode *getRoot() { return root; }
+	BpTreeNode *searchDataNode(string name);
+	void searchRange(string start, string end);
 };
 
 #endif
